9c150315fbf33678d86abc5ec2f86d578f387f4f
Luke Childs <lukechilds123@gmail.com>
2021-03-16 21:09:53 +0700

    Improvements

--- logic/lnd-unlocker.js
+++ logic/lnd-unlocker.js
@@ -1,24 +1,32 @@
 const lightningLogic = require('logic/lightning');
 
-const delay = ms => new Promise(resolve => setTimeout(resolve, ms));
+const SECONDS = 1000;
+const MINUTES = 60 * SECONDS;
 
-const ONE_SECOND = 1000;
-const INTERVAL = 10 * ONE_SECOND;
+const delay = ms => new Promise(resolve => setTimeout(resolve, ms));
 
 module.exports = class LndUnlocker {
   constructor(password) {
     this.password = password;
     this.running = false;
+    this.unlocked = false;
   }
 
   async unlock() {
     try {
-      await lightningLogic.unlockWallet(this.password);
-      console.log('LndUnlocker: Wallet unlocked!');
+      await lndService.getInfo();
+      return true;
     } catch (e) {
-      console.log(`LndUnlocker: Couldn't unlock wallet`);
+      try {
+        await lightningLogic.unlockWallet(this.password);
+        console.log('LndUnlocker: Wallet unlocked!');
+        return true;
+      } catch (e) {
+        console.log('LndUnlocker: Wallet failed to unlock!');
+        return false;
+      }
     }
-  }
+  };
 
   async start() {
     if (this.running) {
@@ -26,12 +34,13 @@ module.exports = class LndUnlocker {
     }
     this.running = true;
     while (this.running) {
-      await this.unlock();
-      await delay(INTERVAL);
+      this.unlocked = await this.unlock();
+      await delay(this.unlocked ? 1 * MINUTES : 10 * SECONDS);
     }
   }
 
   stop() {
     this.running = false;
+    this.unlocked = false;
   }
 }
